c03b41833a67a5dd9b948aada64efeb51a6d9229
Roberto Nicoletti <roberto.urias@gmail.com>
2016-08-08 23:53:51 -0300

    Montando sprite

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/tools/emulador-dimensoes/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
